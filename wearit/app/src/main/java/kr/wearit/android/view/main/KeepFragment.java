9a2bf6badf5fb8235ad26f13e659c5d389c7a5a1
JINSEONG KIM <wlstjd7375@gmail.com>
2016-09-26 18:12:43 +0900

    pull

---
+++ wearit/app/src/main/java/kr/wearit/android/view/main/KeepFragment.java
@@ -0,0 +1,23 @@
+package kr.wearit.android.view.main;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kr.wearit.android.R;
+
+/**
+ * Created by KimJS on 2016-09-26.
+ */
+public class KeepFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_keep, container, false);
+
+        return view;
+    }
+}
