6fdecca1b30ba9a31e2c041f9a706f5f5c22bc46
Parkjehyun <lovegjgjgj@gmail.com>
2016-09-27 19:43:43 +0900

    push test

--- wearit/app/src/main/java/kr/wearit/android/view/product/ProductActivity.java
+++ wearit/app/src/main/java/kr/wearit/android/view/product/ProductActivity.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
