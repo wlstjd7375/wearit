020709d71673aa85e6b5fe3e88600645dc97a820
JINSEONG KIM <wlstjd7375@gmail.com>
2016-10-01 21:01:39 +0900

    start DateTimePicker

---
+++ wearit/app/src/main/java/kr/wearit/android/view/check/CheckBaseActivity.java
@@ -0,0 +1,56 @@
+package kr.wearit.android.view.check;
+
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+
+/**
+ * Created by KimJS on 2016-10-01.
+ */
+
+//Date Time Picker Dialog 기능을 하게 해줌
+public class CheckBaseActivity extends AppCompatActivity implements
+        View.OnClickListener, TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+    private String TAG = "CheckBaseActivity##";
+
+    private DatePickerDialog  dpd;
+
+
+    public void showDialog() {
+        //현재 날짜를 기준으로 dialog 생성
+        Calendar now = Calendar.getInstance();
+        dpd = DatePickerDialog.newInstance(
+                CheckBaseActivity.this, now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH)
+        );
+
+        //세팅
+
+
+        //show
+        dpd.show(getFragmentManager(), "Datepickerdialog");
+    }
+
+
+
+    //Callback 함수
+    @Override
+    public void onClick(View v) {
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        Log.d(TAG, date);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+    }
+}
--- wearit/app/src/main/java/kr/wearit/android/view/main/MyPageFragment.java
+++ wearit/app/src/main/java/kr/wearit/android/view/main/MyPageFragment.java
@@ -1,18 +1,20 @@
 package kr.wearit.android.view.main;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import java.util.ArrayList;
 
@@ -26,8 +28,12 @@ import kr.wearit.android.view.MainActivity;
 /**
  * Created by KimJS on 2016-09-17.
  */
-public class MyPageFragment extends Fragment {
+public class MyPageFragment extends Fragment implements
+        View.OnClickListener,
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener{
 
+    private String TAG = "MyPageFragment##";
     private Context mContext;
 
     private TextView tvToolbarTitle;
@@ -36,6 +42,20 @@ public class MyPageFragment extends Fragment {
     private MyPageAdapter mAdapter;
 
     private String[] mypagetList = {"MY INFO", "ORDER LIST", "COUPON", "SETTING", "CUSTOMER SERVICE", "LOG OUT"};
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        Log.d(TAG, date);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+    }
 
     @Nullable
     @Override
@@ -62,7 +82,10 @@ public class MyPageFragment extends Fragment {
         lvMyPage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //0 부터 시작
                 switch (position) {
+                    case 4:
+                        break;
                     case 5: //LOG OUT api 필요한지
                         UserApi.logout(new Api.OnWaitListener<Void>(getActivity()) {
                             @Override
